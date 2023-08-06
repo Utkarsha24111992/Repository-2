070ac8a2e464570ca52bda679c5e66346276de9b
Utkarsha24111992 <utkarsha.pawar2404@gmail.com>
2023-08-06 11:40:56 +0530

    Second Commit Addition

---
+++ Demo111.java
@@ -0,0 +1,15 @@
+package packagegit2;
+
+public class Demo111 {
+	public static void main(String[] args) {
+	
+		int a=10;
+		int b=20;
+		int c;
+		c=a+b;
+		System.out.println("Addition of two numbers is : " +c);
+	}
+	
+	
+
+}
---
+++ Demo123.java
@@ -0,0 +1,16 @@
+package packagegit2;
+
+public class Demo123 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Test1");
+		System.out.println("Test2");
+		System.out.println("Commit");
+		System.out.println("my first commit"); 
+	          
+	
+	}
+ 
+}
